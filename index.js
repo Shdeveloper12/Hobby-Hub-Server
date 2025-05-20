52679769b93304c479bcaef257a9b606533ecf51
Shdevloper <shafayet237@gmail.com>
2025-05-21 02:16:35 +0600

    added allgroup data to mongodb server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-require ('dotenv').config();
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
@@ -26,9 +26,32 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // create file to mongodb server
+        const groupCollection = client.db('hobbyHubDB').collection('allgroups');
+        app.get('/allgroups', async (req, res) => {
+            const result = await groupCollection.find().toArray();
+            res.send(result);
+        })
+        // allgroups data send to mongodb server
+        app.post('/allgroups', async (req, res) => {
+            const groupData = req.body;
+            console.log(groupData);
+            const result = await groupCollection.insertOne(groupData);
+            res.send(result);
+        })
+
+        // allgroups data received to client side from mongodb
+        app.get('/allgroups/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await groupCollection.findOne(query)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -37,6 +60,8 @@ async function run() {
         // await client.close();
     }
 }
+
+
 run().catch(console.dir);
 
 
