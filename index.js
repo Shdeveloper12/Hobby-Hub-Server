2216104638be9b1d45ad2c42e6f143fc1805ffc1
Shdevloper <shafayet237@gmail.com>
2025-06-11 22:26:47 +0600

    re alignment code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb'); //Added ObjectId
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -12,73 +12,75 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dgti16b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 async function run() {
-    try {
-        // await client.connect();
+  try {
+    // await client.connect();
 
-        const groupCollection = client.db('hobbyHubDB').collection('allgroups');
+    const groupCollection = client.db("hobbyHubDB").collection("allgroups");
 
-        //  GET all groups 
-        app.get('/allgroups', async (req, res) => {
-            const email = req.query.email;
-            const query = email ? { email: email } : {};
-            const result = await groupCollection.find(query).toArray();
-            res.send(result);
-        });
-        app.get('/allgroups/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await groupCollection.findOne(query)
-            res.send(result)
-        })
-        //  POST new group
-        app.post('/allgroups', async (req, res) => {
-            const groupData = req.body;
-            const result = await groupCollection.insertOne(groupData);
-            res.send(result);
-        });
+    //  GET all groups
+    app.get("/allgroups", async (req, res) => {
+      const email = req.query.email;
+      const query = email ? { email: email } : {};
+      const result = await groupCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/allgroups/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await groupCollection.findOne(query);
+      res.send(result);
+    });
+    //  POST new group
+    app.post("/allgroups", async (req, res) => {
+      const groupData = req.body;
+      const result = await groupCollection.insertOne(groupData);
+      res.send(result);
+    });
 
-        // PUT update group
-        app.put('/allgroups/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
-            const options = { upsert: true };
-            const updatedGroup = req.body;
-            const updatedDoc = {
-                $set: updatedGroup,
-            };
-            const result = await groupCollection.updateOne(filter, updatedDoc, options);
-            res.send(result);
-        });
+    // PUT update group
+    app.put("/allgroups/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedGroup = req.body;
+      const updatedDoc = {
+        $set: updatedGroup,
+      };
+      const result = await groupCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
 
-        // DELETE group
-        app.delete('/allgroups/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await groupCollection.deleteOne(query);
-            res.send(result);
-        });
-        console.log(" Connected to MongoDB");
-    
-        
-    } finally {
-        // await client.close(); // keep alive for now
-    }
+    // DELETE group
+    app.delete("/allgroups/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await groupCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    console.log(" Connected to MongoDB");
+  } finally {
+  }
 }
 
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('My hobby website backend is running');
+app.get("/", (req, res) => {
+  res.send("My hobby website backend is running");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
